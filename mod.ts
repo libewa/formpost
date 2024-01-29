24eca322ad5dd2a5e55a283103b56c630bc809e5
Linus <67926131+libewa@users.noreply.github.com>
2024-01-29 10:46:06 +0100

    await

--- mod.ts
+++ mod.ts
@@ -8,7 +8,7 @@ const mno = new Response("This route currently only accepts POST requests", {
 
 Deno.serve({ port: 80, hostname: "0.0.0.0" }, (_req) => {
     if (_req.method == "POST") {
-        console.log(_req.formData())
+        console.log(await _req.formData())
         return res;
     } else return mno;
 });
