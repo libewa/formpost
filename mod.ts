e3a293073ed4c86a2bbdd4854ab38070f57153b7
Linus <67926131+libewa@users.noreply.github.com>
2024-01-29 10:41:52 +0100

    real initial

---
+++ mod.ts
@@ -0,0 +1,14 @@
+const res = new Response("Resource created", {status: 201});
+const mno = new Response("This route currently only accepts POST requests", {
+    status: 405,
+    headers: {
+        "Allow": "POST"
+    }
+});
+
+Deno.serve({ port: 80, hostname: "0.0.0.0" }, (_req) => {
+    if (_req.method == "POST") {
+        Deno.writeTextFile("./form.txt", _req.formData());
+        return res;
+    } else return mno;
+});
